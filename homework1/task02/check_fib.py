5014d2f945f8d480132e91913387029e14945913
Astony <savrushkin.aa@gmail.com>
2021-06-24 21:03:52 +0300

    I improve the task02 after lection about generators

--- homework1/task02/check_fib.py
+++ homework1/task02/check_fib.py
@@ -1,26 +1,16 @@
-from math import sqrt
 from typing import Sequence
 
 
-def check_fibonacci(x: Sequence[int]) -> bool:
-    if len(x) != 0:
-        if sqrt(5 * (x[0] ** 2) - 4) % 1 == 0 or sqrt(5 * (x[0] ** 2) + 4) % 1 == 0:
-            fib1 = fib2 = 1
-            while fib2 != x[0]:
-                fib = fib1 + fib2
-                fib1 = fib2
-                fib2 = fib
-            for i in range(len(x)):
-                if x[i] == 1 and x[i] == x[i + 1]:
-                    continue
-                elif x[i] == fib2:
-                    fib = fib1 + fib2
-                    fib1 = fib2
-                    fib2 = fib
-                else:
-                    return False
-            return True
-        else:
+def fib_generator():
+    fib1, fib2 = 0, 1
+    while True:
+        yield fib1
+        fib1, fib2 = fib2, fib1+fib2
+
+
+def check_fibonacci(inp_seq):
+    gen = fib_generator()
+    for element in inp_seq:
+        if element != next(gen):
             return False
-    else:
-        return False
+    return True
--- homework1/task02/tests/test_check_fib.py
+++ homework1/task02/tests/test_check_fib.py
@@ -1,13 +1,10 @@
 from homework1.task02.check_fib import check_fibonacci
 
 
+
 def test_right_sequence():
-    assert check_fibonacci([1, 1, 2, 3, 5, 8])
-    assert check_fibonacci([3, 5, 8, 13, 21])
-    assert check_fibonacci([1, 2, 3, 5, 8])
+    assert check_fibonacci([0, 1, 1, 2, 3, 5, 8, 13])
 
 
 def test_wrong_sequence():
-    assert not check_fibonacci([1, 1, 2, 3, 5, 7])
-    assert not check_fibonacci([4, 5, 8, 13, 21])
-    assert not check_fibonacci([7, 5, 3, 2, 1, 1])
+    assert not check_fibonacci([0, 1, 1, 2, 3, 5, 7])
