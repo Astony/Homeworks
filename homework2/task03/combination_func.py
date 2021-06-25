3d7574442da8864624c50ecefceefa2bee5e27d1
Astony <savrushkin.aa@gmail.com>
2021-06-26 01:06:09 +0300

    task03 with combination func

---
+++ homework2/task03/combination_func.py
@@ -0,0 +1,15 @@
+import itertools
+from typing import Any, List
+
+
+def combinations(list_of_lists: List[Any]) -> List[List]:
+    all_elements = list(itertools.chain(*list_of_lists))
+    combos = list(itertools.combinations(all_elements, len(list_of_lists)))
+    iterator = 0
+    while iterator != len(list_of_lists):
+        sorted_list_of_combinations = [
+            i for i in combos if i[iterator] in list_of_lists[iterator]
+        ]
+        combos = sorted_list_of_combinations[:]
+        iterator += 1
+    return list(map(list, sorted_list_of_combinations))
