cd25c0cdb2d94b42d0aca2ee48efe35c522ccc01
Astony <savrushkin.aa@gmail.com>
2021-06-28 19:01:43 +0300

    modify test file

--- homework2/tests/test_work_with_text_funct_t05.py
+++ homework2/tests/test_work_with_text_funct_t05.py
@@ -1,12 +1,8 @@
-from homework2.task01.work_with_text import (count_non_ascii_chars,
-                                             count_punctuation_chars,
-                                             get_longest_diverse_words,
-                                             get_most_common_non_ascii_char,
-                                             get_rarest_char)
+from homework2.task01 import work_with_text
 
 
 def test_get_longest_diverse_words():
-    assert get_longest_diverse_words("data.txt") == [
+    assert work_with_text.get_longest_diverse_words("data.txt") == [
         "ÜBERSICHT",
         "Symbol",
         "vollzo",
@@ -21,16 +17,16 @@ def test_get_longest_diverse_words():
 
 
 def test_get_rarest_char():
-    assert get_rarest_char("data.txt") == "›"
+    assert work_with_text.get_rarest_char("data.txt") == "›"
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars("data.txt") == 5392
+    assert work_with_text.count_punctuation_chars("data.txt") == 5392
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars("data.txt") == 2972
+    assert work_with_text.count_non_ascii_chars("data.txt") == 2972
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char("data.txt") == "ä"
+    assert work_with_text.get_most_common_non_ascii_char("data.txt") == "ä"
