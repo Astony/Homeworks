1ebcfb816a0176805429aad3593dfa3fc4fbab0d
Astony <savrushkin.aa@gmail.com>
2021-06-30 02:23:18 +0300

    final chord

--- homework1/task05/sum_of_subarray.py
+++ homework1/task05/sum_of_subarray.py
@@ -1,14 +1,14 @@
 from typing import List
 
 
-def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
+def find_maximal_subarray_sum(nums: List[int], sub_len: int) -> int:
     """Exclude case with zero or negative cases
     In this cases there is 0 on output"""
-    if 0 < k <= len(nums) != 0:
+    if 0 < sub_len <= len(nums) != 0:
         """Compare all next subarrays with the first"""
-        sum_subarray = sum(nums[:k])
-        for i in range(len(nums) - (k - 1)):
-            if sum_subarray < sum(nums[i : k + i]):
-                sum_subarray = sum(nums[i : k + i])
+        sum_subarray = sum(nums[:sub_len])
+        for i in range(len(nums) - (sub_len - 1)):
+            if sum_subarray < sum(nums[i : sub_len + i]):
+                sum_subarray = sum(nums[i : sub_len + i])
         return sum_subarray
     return 0
