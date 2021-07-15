db185ab91fe795e62be00d19d97f36e98531c8fe
Astony <savrushkin.aa@gmail.com>
2021-07-15 16:06:17 +0300

    fix black_isort

--- homework6/tests/test_for_classes.py
+++ homework6/tests/test_for_classes.py
@@ -1,4 +1,5 @@
 import pytest
+
 from homework6.task02.hard_classes_task import (DeadlineError, Homework,
                                                 HomeworkResult, Student,
                                                 Teacher)
