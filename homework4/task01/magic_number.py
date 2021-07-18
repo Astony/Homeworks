bc276bae69cdc725f21b4f1455d550ef924209bb
Astony <savrushkin.aa@gmail.com>
2021-07-18 22:31:22 +0300

    I have made separated function for reading line from txt

--- homework4/task01/magic_number.py
+++ homework4/task01/magic_number.py
@@ -4,10 +4,15 @@ is int, otherwise if it has another type, func return ValueError
 """
 
 
-def read_magic_number(path: str) -> bool:
+def line_reader_func(path: str) -> str:
     with open(path, "r") as file:
-        try:
-            line = file.readline().strip()
-            return 1 <= float(line) < 3
-        except Exception as err:
-            raise ValueError("It is not a magic number!") from err
+        line = file.readline().strip()
+    return line
+
+
+def read_magic_number(path: str) -> bool:
+    line = line_reader_func(path)
+    try:
+        return 1 <= float(line) < 3
+    except Exception as err:
+        raise ValueError("It is not a magic number!") from err
