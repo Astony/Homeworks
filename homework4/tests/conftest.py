702a2d2322dbd2b07ca34467e30082843f0f5adf
Astony <savrushkin.aa@gmail.com>
2021-07-19 11:03:50 +0300

    forgot about context manager

--- homework4/tests/conftest.py
+++ homework4/tests/conftest.py
@@ -14,10 +14,10 @@ def file_with_true_and_false_value(inp):
     """
     Create file and write into True or False cases
     """
-    test_file = open(file, "w+")
-    test_file.write(inp)
-    test_file.close()
-    yield file
+    with open(file, "w+") as test_file:
+        test_file.write(inp)
+        test_file.close()
+        yield file
     os.remove(file)
 
 
@@ -26,19 +26,19 @@ def file_with_exception_value():
     """
     Fixture for exception case
     """
-    test_file = open(file, "w+")
-    test_file.write("1 2 3 5")
-    test_file.close()
-    yield file
+    with open(file, "w+") as test_file:
+        test_file.write("1 2 3 5")
+        test_file.close()
+        yield file
     os.remove(file)
 
 
 @pytest.fixture()
 def exist_of_file():
     """Check the existence of file after failed test"""
-    test_file = open(file, "w+")
-    test_file.write("3")
-    test_file.close()
-    yield file
+    with open(file, "w+") as test_file:
+        test_file.write("3")
+        test_file.close()
+        yield file
     os.remove(file)
     assert not os.path.exists(file)
