6d19328991f109ed18b8c434686dbac8b8d863d4
Astony <savrushkin.aa@gmail.com>
2021-08-11 14:13:55 +0300

    final version

--- homework9/task01/merge_sorted_files.py
+++ homework9/task01/merge_sorted_files.py
@@ -1,24 +1,17 @@
-from itertools import zip_longest
-from pathlib import Path
+from contextlib import ExitStack
+from itertools import chain, zip_longest
 from typing import Generator, List
-
-
-def numbers_generator_from_file(file_name: str) -> Generator[int, None, None]:
-    """Generator that returns numbers from file one by one"""
-    path = Path.cwd() / f"{file_name}"
-    return (int(number) for number in path.read_text().split() if number != "\n")
 
 
 def merge_sorted_files(file_list: List) -> Generator[int, None, None]:
     """Generator that merge two lists"""
-    try:
-        pairs_from_lists = zip_longest(
-            numbers_generator_from_file(file_list[0]),
-            numbers_generator_from_file(file_list[1]),
-        )
-        for pair in pairs_from_lists:
-            for element in pair:
-                if element:
-                    yield element
-    except ValueError:
-        print("Files should have only integers")
+    with ExitStack() as stack:
+        files = [stack.enter_context(open(fname)) for fname in file_list]
+        file1, file2 = files[0].readlines(), files[1].readlines()
+        try:
+            for element in (
+                element for element in chain(*zip_longest(file1, file2)) if element
+            ):
+                yield int(element.strip())
+        except ValueError:
+            print("Files should have only integers")
--- homework9/task03/file_counter.py
+++ homework9/task03/file_counter.py
@@ -1,16 +1,12 @@
 from pathlib import Path
 
 
-def default_tokenizer(text: str):
-    """Tokenizer that counts lines in file"""
-    text = [line for line in text.split("\n")]
-    return text
-
-
-def universal_file_counter(path_dir: str, extension: str, tokenizer=default_tokenizer):
+def universal_file_counter(path_dir: str, extension: str, tokenizer=None):
     """File counter, that will count lines in all files with that extension
     if there are no tokenizer. If a the tokenizer is not none, it will count tokens."""
     directory_path = Path.cwd() / f"{path_dir}"
+    if not tokenizer:
+        tokenizer = lambda x: x.split("\n")
     if directory_path.exists():
         text = (
             tokenizer(path.read_text())
