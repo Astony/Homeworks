0525bb1b741fba80a0fba52c39b6e5bef0712b86
Astony <savrushkin.aa@gmail.com>
2021-08-17 12:56:38 +0300

    new project

--- homework12/project/orm/apps.py
+++ homework12/project/orm/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OrmTaskConfig(AppConfig):
+class OrmConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "orm_task"
+    name = "orm"
--- homework12/project/project/asgi.py
+++ homework12/project/project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hw12 project.
+ASGI config for project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hw12.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_asgi_application()
