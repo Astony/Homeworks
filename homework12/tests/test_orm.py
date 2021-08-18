c5f944a4a987b6d33b5c2b2e970d5219ec45808a
Astony <83858616+Astony@users.noreply.github.com>
2021-08-18 11:30:35 +0300

    Update test_orm.py

--- homework12/tests/test_orm.py
+++ homework12/tests/test_orm.py
@@ -3,23 +3,28 @@ import pytest
 from homework12.project.orm.models import Homework, HomeworkResult, Student, Teacher
 
 
+@pytest.mark.parametrize(
+    "record, result",
+    [
+        ("teacher.first_name", "John"),
+        ("homework.creator.first_name", "John"),
+        ("student.first_name", "Joe"),
+        ("hw_result.creator.first_name", "Joe"),
+    ],
+)
 @pytest.mark.django_db
-def test_databases(create_tables):
+def test_positive_cases(create_tables, record, result):
     """Test that all created records exist in test db"""
 
     teacher = Teacher.objects.get(first_name="John")
     homework = Homework.objects.get(creator=teacher.id)
     student = Student.objects.get(first_name="Joe")
     hw_result = HomeworkResult.objects.get(creator=student.id)
-
-    assert teacher.first_name == "John"
-    assert homework.creator.first_name == "John"
-    assert student.first_name == "Joe"
-    assert hw_result.creator.first_name == "Joe"
+    assert eval(record) == result
 
 
 @pytest.mark.django_db
-def test_negative__db(create_tables):
+def test_negative_case(create_tables):
     """Test that attempt to get an empty query throw an exception"""
     with pytest.raises(Exception):
         teacher = Teacher.objects.get(first_name="Incognito")
